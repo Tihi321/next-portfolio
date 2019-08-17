1b40f371737e40f999e044fe99f1b7b39ef3066c
Tihomir Selak <tihomir.selak@infinum.hr>
2019-08-17 18:10:21 +0200

    add head element

--- pages/web/index.js
+++ pages/web/index.js
@@ -1,9 +1,10 @@
-import Link from 'next/link';
 import Navbar from '../../components/Navbar';
+import Head from '../../components/Head';
 
 export default () => {
   return (
     <>
+      <Head title="Tihomir Selak Portfolio" />
       <Navbar />
       <div>Web</div>
     </>
