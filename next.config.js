71e155426a615fc633c79d3770e3efed80526649
Tihomir Selak <tihomir.selak@infinum.hr>
2019-08-17 22:10:17 +0200

    add scss source maps

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 const withSass = require('@zeit/next-sass');
 module.exports = withSass({
-  cssModules: true
+  cssModules: true,
+  sassLoaderOptions: {
+    sourceMap: true
+  },
+  postcssLoaderOptions: {
+      sourceMap: true
+  }
 });
