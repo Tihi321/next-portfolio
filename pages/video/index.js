05ab920b8a3d107f1042130b8ce9525d5e817490
Tihomir Selak <tihomir.selak@infinum.hr>
2019-08-18 18:41:35 +0200

    Add core style libs and config

--- pages/video/index.js
+++ pages/video/index.js
@@ -2,8 +2,6 @@ import Navbar from '../../components/Navbar';
 import Head from '../../components/Head';
 
 export default (props) => {
-
-  console.log(props);
   return (
     <>
       <Head title="Video Development" />
