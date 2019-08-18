500d24c4c56d74aebde7187fac98b494ccd7e08e
Tihomir Selak <tihomir.selak@infinum.hr>
2019-08-18 18:17:02 +0200

    add enviroment development

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,26 @@ import 'isomorphic-fetch';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
+
+    const https = require("https");
+    
+    // enable self signed ceriticate api call for development.
+    const development = (process.env.NEXT_ENV === 'development')
+
+    const options = {
+      agent: new https.Agent({
+        rejectUnauthorized: !development
+      })
+    };
+
     let pageProps = {};
 
-    const res = await fetch('http://localhost:3000/api/data')
+    const dataUrl = 'https://blog.tihomir-selak.from.hr/?rest_route=/portfolio-backend/v1/portfolio-page';
+
+    const res = await fetch(dataUrl, options)
     const json = await res.json()
     pageProps.data = json;
+    pageProps.env = process.env;
     return { pageProps };
   }
 
---
+++ pages/api/posts/[pid].js
@@ -0,0 +1,21 @@
+import Cors from 'micro-cors';
+import { posts } from '../data/posts';
+
+const cors = Cors({
+  allowedMethods: ['GET'],
+});
+
+function Endpoint(req, res) {
+  const {
+    query: { pid },
+  } = req;
+
+  const post = posts.filter((item) => `${item.id}` === pid);
+  const output = (post.length > 0) ? post : {
+    error: 'Post not found',
+  };
+
+  res.json(output);
+}
+
+export default cors(Endpoint);
