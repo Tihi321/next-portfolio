93170f87fee1422ff23e7e43bcd41e1c09d6f7bf
Tihomir Selak <tihomir.selak@infinum.hr>
2019-08-20 00:49:45 +0200

    add portfolio icon fonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,27 @@
 const postcssFontMagician = require('postcss-font-magician');
 const autoPrefixer = require('autoprefixer');
 
+const fontsPath = `/static/fonts`;
+
 module.exports = {
   plugins: [
     autoPrefixer,
     postcssFontMagician({
+      custom: {
+        'portfolio': {
+          variants: {
+            normal: {
+              400: {
+                url: {
+                  svg: `${fontsPath}/portfolio.svg`,
+                  ttf: `${fontsPath}/portfolio.ttf`,
+                  woff: `${fontsPath}/portfolio.woff`,
+                },
+              },
+            },
+          },
+        },
+      },
       variants: {
         'Open Sans Condensed': {
           400: ['woff2', 'woff'],
@@ -16,7 +33,7 @@ module.exports = {
         },
       },
       display: 'swap',
-      foundries: [ 'google'],
+      foundries: [ 'custom', 'google' ],
     })
   ]
 };
