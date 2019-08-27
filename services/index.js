b2a5c0a9a80b45debb6e552f106b30dc3e30fc99
Tihomir Selak <tihomir.selak@infinum.hr>
2019-08-27 23:14:23 +0200

    removed snapshot

--- services/index.js
+++ services/index.js
@@ -2,16 +2,16 @@ import { Client } from './client';
 
 const __API_CLIENT__ = 'client';
 
-export function getOrCreateClient(snapshot) {
+export function getOrCreateClient() {
   // Always make a new store if server, otherwise state is shared between requests
 
   if (typeof window === 'undefined') {
-    return new Client(snapshot);
+    return new Client();
   }
 
   // This will be true when page constructor is called on client
   if (!window[__API_CLIENT__]) {
-    window[__API_CLIENT__] = new Client(snapshot);
+    window[__API_CLIENT__] = new Client();
   }
 
   return window[__API_CLIENT__];
