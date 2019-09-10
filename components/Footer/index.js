b3c5ac199662309741d4fce8a73cdc082ef44a92
Tihomir Selak <tihomir.selak@infinum.hr>
2019-09-10 19:17:14 +0200

    add wordpress utl

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Container from '../Container';
-import {footerClass} from './style.scss';
+import SocialBar from '../SocialBar';
+import {
+  footerClass,
+  contentClass,
+  socialIconClass,
+} from './style.scss';
 
 const Footer = ({options, date}) => {
 
   return (
     <footer className={footerClass}>
       <Container>
-        <span>{`@${date} Tihomir Selak`}</span>
+        <div className={contentClass}>
+          <span>{`@${date} Tihomir Selak`}</span>
+          <SocialBar
+            options={options}
+            className={socialIconClass}
+          />
+      </div>
       </Container>
     </footer>
   );
